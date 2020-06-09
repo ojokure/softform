64434d062da1cca89caf6e54a1aceacc6dcf5860
oladimeji-ojo <ojokuredim@gmail.com>
2020-06-09 14:32:16 +0100

    scaffold a barebone form

--- reactForm/src/Form.js
+++ reactForm/src/Form.js
@@ -1,8 +1,76 @@
 import React, { useState } from "react";
+import {
+  validateName,
+  validateEmail,
+  validatePhoneNumber,
+  validatePassword,
+  validateConfirmPass,
+  validateCardNumber,
+  validateExpiryDate,
+  validatePin,
+} from "./validators";
 
 const Form = () => {
-  const [inputState, setInputState] = useState();
-  return <div> "Form Ahoy" </div>;
+  const formValue = {
+    fullName: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    confirmPassword: "",
+    cardNumber: "",
+    expiryDate: "",
+    pin: "",
+  };
+
+  const [inputState, setInputState] = useState(formValue);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setInputState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    console.log(inputState);
+  };
+
+  return (
+    <form onChange={handleChange}>
+      <div>
+        <label>Name</label>
+        <input
+          name="fullName"
+          className={`form-control ${inputState.nameError ? "is-invalid" : ""}`}
+          id="name"
+          placeholder="Enter name"
+          defaultValue={inputState.name}
+          onBlur={validateName}
+        />
+        <div>{inputState.nameError}</div>
+      </div>
+      <div>
+        <label>Email</label>
+        <input
+          name="email"
+          className={`form-control ${
+            inputState.emailError ? "is-invalid" : ""
+          }`}
+          id="email"
+          placeholder="Enter email"
+          defaultValue={inputState.email}
+          onBlur={validateEmail}
+        />
+        <div>{inputState.emailError}</div>
+      </div>
+      <button type="submit" onClick={handleSubmit}>
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default Form;
---
+++ reactForm/src/validators.js
@@ -0,0 +1,8 @@
+export const validateName = () => {};
+export const validateEmail = () => {};
+export const validatePhoneNumber = () => {};
+export const validatePassword = () => {};
+export const validateConfirmPass = () => {};
+export const validateCardNumber = () => {};
+export const validateExpiryDate = () => {};
+export const validatePin = () => {};
